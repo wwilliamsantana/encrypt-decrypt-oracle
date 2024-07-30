e207eef7a386754e38644fbdfae1faacfb3264b1
William Santana <euwilliamsantana@gmail.com>
2024-07-30 20:35:29 -0300

    Inital commit: Finished Project

---
+++ main.js
@@ -0,0 +1,48 @@
+const textTextarea = document.getElementsByTagName("textarea")[0]
+const containerResult = document.getElementsByClassName("content-result")[0]
+let simpleCrypto = new SimpleCrypto("NextEducationOne")
+
+
+function encryptOrDecrypt(action) {
+  containerResult.innerHTML = ""
+  containerResult.classList.add("result-completed")
+
+  const paragraph = document.createElement("p")
+  paragraph.className = 'content-result-text'
+
+  if (action == "encrypt") {
+    paragraph.innerText = simpleCrypto.encrypt(textTextarea.value)
+  } else {
+    paragraph.innerText = simpleCrypto.decrypt(textTextarea.value)
+  }
+
+  const button = document.createElement("button")
+  button.innerText = "Copiar"
+  button.className = "content-result-button"
+  button.onclick = copyContent
+
+  containerResult.appendChild(paragraph)
+  containerResult.appendChild(button)
+
+  textTextarea.value = ""
+}
+
+function onEncrypt() {
+  encryptOrDecrypt("encrypt")
+}
+
+function onDecrypt() {
+  encryptOrDecrypt("decrypt")
+}
+
+function copyContent() {
+  const textCopy = document.querySelector(".content-result-text")
+
+  navigator.clipboard.writeText(textCopy.textContent).then(() => {
+    textCopy.textContent = "Texto Copiado com sucesso!"
+    textCopy.classList.add("success")
+  }).catch(err => {
+    textCopy.textContent = `Erro ao copiar o texto: ${err}`
+    textCopy.classList.add("failure")
+  })
+}
